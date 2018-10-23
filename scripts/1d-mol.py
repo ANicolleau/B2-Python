443c1f160c8caf52b4ca9fbfdb35d85cd19d636f
Antoine Nicolleau <antoine.nicolleau@ynov.com>
2018-10-23 13:05:05 +0200

    scripts

--- scripts/1d-mol.py
+++ scripts/1d-mol.py
@@ -4,19 +4,40 @@
 #Antoine Nicolleau
 #23 oct 2018
 
+#Importation
+import random
+import sys
+import signal
+
+#Fonction permettant de quitter le jeu et de donner la réponse
+def endGame(): 
+    print("Perdu ! La solution était " + str(rdm) + ". A Bientôt ! ")
+    sys.exit(0)
+    return
+
+# Fonction pour quitter joliment avec CTRL+C
+def leaver (sig, frame):
+    print('Tu as fait CTRL+C, la soltuion était ' + str(rdm))
+    sys.exit(0)
+   
+
+
+signal.signal(signal.SIGINT, leaver)
+
 # Initialisation du nombre random entre 1 et 100 
 # et de la variable qui va contenir la saisie utilisateur
-import random
 rdm = random.randint(1,100)
-
 print (rdm)
-
 nbuser =input('Saisir un nombre entre 1 et 100 ? ')
+
 # Tant que ma saisie utilisateur est différente de mon nombre rdm,
 #boucler jusqu'à ce que l'utilisateur trouve le nombre.
-while int(nbuser) != int(rdm) or str(nbuser) != q:
+while str(nbuser) != str(rdm):
     
-    if int(nbuser) < int(rdm):
+    if str(nbuser) == 'q':
+        endGame()
+    
+    elif int(nbuser) < int(rdm):
         print("C'est plus")
     
     elif int(nbuser) > int(rdm) and int(nbuser) > 0 and int(nbuser) < 100:
@@ -29,3 +50,4 @@ while int(nbuser) != int(rdm) or str(nbuser) != q:
 
 if int(nbuser) == int(rdm):
     print("Bravo tu as gagné ! La solution était bien " + nbuser + ". A Bientôt !")
+
