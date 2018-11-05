283c2b72b76ad7bd96bc3158a20400db83840792
Antoine Nicolleau <antoine.nicolleau@ynov.com>
2018-11-05 20:54:05 +0100

    bug 2a

--- scripts/2a-mol/2a-mol.py
+++ scripts/2a-mol/2a-mol.py
@@ -12,24 +12,18 @@ import time
 rdm = random.randint(1,100)
 nbuser = -1
 filetest = '2atest.txt'
-welcome = 'Bonjour et bienvenu dans ce jeu du plus ou du moins. Pour commencer le jeu, entrer un nombre'
+welcome = 'Bonjour et bienvenu dans ce jeu du plus ou du moins. Pour commencer le jeu, entrez un nombre entre 0 et 100 sur la 2e ligne'
 plus = 'C\'est plus'
 moins = 'C\'est moins'
-win = 'C\'est gagné, le nombre était bien : '
+win = 'C\'est gagné ! '
 rejouer = 'Entrez un nombre entre 1 et 100'
 
 
+# fonction qui me permet d'écrire dans les fichiers
 def write(file,msg):
         filetmp = open(file, "w")
         filetmp.write(msg)
         filetmp.close
-
-def read(file):
-        open(file, "r")
-
-
-
-
 
 write(filetest, welcome)
 
@@ -40,22 +34,20 @@ write(filetest, welcome)
     # Si ce n'est pas un nombre 
 while int(nbuser) != int(rdm):
         gamefile = open(filetest, "r")
-        compteur = 0
-        for line in gamefile.readlines():
-                compteur += 1
-                if compteur == 2:
+        for line in gamefile.readlines()[1]:
+                
 
-                        if int(nbuser) < int(rdm):
-                                write(filetest,plus)
+                if int(nbuser) < int(rdm):
+                        write(filetest, plus)
                         
-                        elif int(nbuser) > int(rdm) :
-                                write(filetest, moins)
+                elif int(nbuser) > int(rdm) :
+                        write(filetest, moins)
                         
-                        elif int(nbuser) < 0 or int(nbuser) > 100: 
-                               write(filetest, rejouer)
+                elif int(nbuser) < 0 or int(nbuser) > 100: 
+                        write(filetest, rejouer)
 
-                        elif int(nbuser) == int(rdm):
-                                write(filetest, win)
+                elif int(nbuser) == int(rdm):
+                        write(filetest, win)
 
         gamefile.close()
         time.sleep(2)
